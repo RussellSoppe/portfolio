1b569ca0010a0c988501e147de961a8977b19133
russellicious <42648750+russellicious@users.noreply.github.com>
2018-10-12 12:02:12 -0500

    quick edits on about banner text

--- src/components/AboutBanner.js
+++ src/components/AboutBanner.js
@@ -6,8 +6,8 @@ const AboutBanner = ()=>{
 		<div>
                <h1>Russell Soppe Web Developer Portfolio</h1>
                <p>Hello, I am not sure how you found this site but I am glad you have.<br/> 
-               This site is a work in progress, both as a portfolio site and some of the sites embedded are still a work in progress.<br/> 
-               If you happened to stumble across this page and aren't impressed yet, check back once a week or so and see what changes have been made. 
+               This site is a work in progress, both as a portfolio site and some of the sites within.<br/> 
+               If you happened to stumble across this page and aren't impressed yet, check every couple of weeks see what changes have been made. 
                </p>
                <p>This portfolio site is being made with React. 
                React was created by Facebook, check it out here at 
