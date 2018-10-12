7358a2cfe5baf2c81218f0737014b03d52501ef4
russellicious <42648750+russellicious@users.noreply.github.com>
2018-10-12 12:05:17 -0500

    fixed typo in AboutBanner

--- src/components/AboutBanner.js
+++ src/components/AboutBanner.js
@@ -7,7 +7,7 @@ const AboutBanner = ()=>{
                <h1>Russell Soppe Web Developer Portfolio</h1>
                <p>Hello, I am not sure how you found this site but I am glad you have.<br/> 
                This site is a work in progress, both as a portfolio site and some of the sites within.<br/> 
-               If you happened to stumble across this page and aren't impressed yet, check every couple of weeks see what changes have been made. 
+               If you happened to stumble across this page and aren't impressed yet, check back every couple of weeks and see what changes have been made. 
                </p>
                <p>This portfolio site is being made with React. 
                React was created by Facebook, check it out here at 
