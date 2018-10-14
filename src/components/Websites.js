ce79af68c625037604556da82183120eb330de4a
russellicious <42648750+russellicious@users.noreply.github.com>
2018-10-14 14:16:07 -0500

    fixed margin issue by changing aos to up and down instead of left and right.

--- src/components/Websites.js
+++ src/components/Websites.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Websites.css';
 
 const Websites = (props) => {
 	
--- src/components/WebsitesMF.js
+++ src/components/WebsitesMF.js
@@ -8,10 +8,10 @@ const WebsitesMF = (props) => {
 
 	          <div className="outercard">
 	            <div className="innercard">
-	              <div data-aos="fade-left">
+	              <div data-aos="fade-up">
 				          <img alt="StarWars" href=""></img>
 	          		</div>
-	          		<div className="cardtext" data-aos="fade-right">
+	          		<div className="cardtext" data-aos="fade-down">
 	              	<h1>Star Wars API</h1>
 		              <p>This website was created with React using the Star Wars API.</p>
 		              <p>Future plans include adding planets and vehicle pages as well as links between within profiles.</p>
@@ -23,10 +23,10 @@ const WebsitesMF = (props) => {
 	        
 	          <div className="outercard">
 	            <div className="innercard">
-	            	<div data-aos="fade-right">
+	            	<div data-aos="fade-up">
 	            		<img alt="React and Clarifai API" href=""></img>
 	              </div>
-	            	<div className="cardtext" data-aos="fade-left">
+	            	<div className="cardtext" data-aos="fade-down">
 	            		<h1>React and Clarifai API</h1>
 		              <p>This website was created with React using the Clarifai API to detect faces in a picture!</p>
 		              <a className="btn" href="https://russellsoppe.github.io/facebrain/" role="button">Go to Website</a>
@@ -36,10 +36,10 @@ const WebsitesMF = (props) => {
 	     
            <div className="outercard">
 	            <div className="innercard">
-	            	<div data-aos="fade-left">
+	            	<div data-aos="fade-up">
             		 	<img alt="Background Generator" href=""></img>
                 </div>
-                <div className="cardtext" data-aos="fade-right">
+                <div className="cardtext" data-aos="fade-down">
 	            		<h1>Background Generator</h1>
 		              <p>This website was created with HTML, CSS, and JavaScript as an exercise in Zero To Mastery Course.</p>
 		              <a className="btn" href="https://russellsoppe.github.io/Background-Generator" role="button">Go to Website</a>
@@ -50,10 +50,10 @@ const WebsitesMF = (props) => {
 		    
 	          <div className="outercard">
 	            <div className="innercard">
-	            	<div data-aos="fade-right">
+	            	<div data-aos="fade-up">
 		            	<img alt="NavBar StartUp" href=""></img>
 	            	</div>
-	            	<div className="cardtext" data-aos="fade-left">
+	            	<div className="cardtext" data-aos="fade-down">
 	            		<h1>NavBar Start Up</h1>
 		              <p>This website was created with HTML, CSS, and JavaScript.</p>
 		              <a className="btn" href="https://russellsoppe.github.io/navbar-startup/" role="button">Go to Website</a>
@@ -63,10 +63,10 @@ const WebsitesMF = (props) => {
 	    
 	    			<div className="outercard">
 	            <div className="innercard">
-	            	<div data-aos="fade-left">
+	            	<div data-aos="fade-up">
             		 	<img alt="StarWars" href=""></img>
                 </div>
-                <div className="Mobile First StartUp" data-aos="fade-right">
+                <div className="Mobile First StartUp" data-aos="fade-down">
 	            		<h1>Mobile First Start Up</h1>
 		              <p>This website was created with HTML, CSS, and JavaScript.</p>
 		              <a className="btn" href="https://russellsoppe.github.io/startupmf/" role="button">Go to Website</a>
@@ -77,10 +77,10 @@ const WebsitesMF = (props) => {
 	                 
 	          <div className="outercard">
 		          <div className="innercard">
-		          	<div data-aos="fade-right">
+		          	<div data-aos="fade-up">
 		              <img alt="Calculator" href=""></img>
 	              </div>
-	            	<div className="cardtext" data-aos="fade-left">
+	            	<div className="cardtext" data-aos="fade-down">
 	            		<h1>DOM Calculator</h1>
 		              <p>This website was created with HTML, CSS, and JavaScript. 
 		              The current style choices are from my daughter who loves all things pink and purple.</p>
