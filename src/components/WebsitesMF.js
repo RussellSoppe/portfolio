3d55280b31ee83e1de1413f7fc93ea49a452b676
russellicious <42648750+russellicious@users.noreply.github.com>
2018-10-14 16:02:28 -0500

    added pics and took out iframes for mobile first component

--- src/components/WebsitesMF.js
+++ src/components/WebsitesMF.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import './Websites.css';
+import facebrain from './images/facebrain.PNG';
+import swapi from './images/swapi.PNG';
+import backgen from './images/backgen.PNG';
+import navbarsu from './images/navbarsu.PNG';
+import mfnavbar from './images/mfnavbar.PNG';
+import calculator from './images/calculator.png';
+
 
 const WebsitesMF = (props) => {
 	
@@ -8,11 +15,14 @@ const WebsitesMF = (props) => {
 
 	          <div className="outercard">
 	            <div className="innercard">
+	            	<div className="cardtexttitle" data-aos="fade-down">
+	              	<h1>Star Wars API</h1>
+		            </div>
 	              <div data-aos="fade-up">
-				          <img alt="StarWars" href=""></img>
+				          <img className="webimg" alt="StarWars" src={swapi}></img>
 	          		</div>
 	          		<div className="cardtext" data-aos="fade-down">
-	              	<h1>Star Wars API</h1>
+	              	
 		              <p>This website was created with React using the Star Wars API.</p>
 		              <p>Future plans include adding planets and vehicle pages as well as links between within profiles.</p>
 		              <a className="btn" href="https://russellsoppe.github.io/StarWars/" role="button">Go to Website</a>
@@ -23,11 +33,14 @@ const WebsitesMF = (props) => {
 	        
 	          <div className="outercard">
 	            <div className="innercard">
+	            	<div className="cardtexttitle" data-aos="fade-down">
+	              	<h1>React and Clarifai API</h1>
+		            </div>
 	            	<div data-aos="fade-up">
-	            		<img alt="React and Clarifai API" href=""></img>
+	            		<img className="webimg" alt="React and Clarifai API" src={facebrain}></img>
 	              </div>
 	            	<div className="cardtext" data-aos="fade-down">
-	            		<h1>React and Clarifai API</h1>
+	            		
 		              <p>This website was created with React using the Clarifai API to detect faces in a picture!</p>
 		              <a className="btn" href="https://russellsoppe.github.io/facebrain/" role="button">Go to Website</a>
 	            	</div>
@@ -36,11 +49,14 @@ const WebsitesMF = (props) => {
 	     
            <div className="outercard">
 	            <div className="innercard">
+	            	<div className="cardtexttitle" data-aos="fade-down">
+	              	<h1>Background Generator</h1>
+		            </div>
 	            	<div data-aos="fade-up">
-            		 	<img alt="Background Generator" href=""></img>
+            		 	<img className="webimg" alt="Background Generator" src={backgen}></img>
                 </div>
                 <div className="cardtext" data-aos="fade-down">
-	            		<h1>Background Generator</h1>
+	            		
 		              <p>This website was created with HTML, CSS, and JavaScript as an exercise in Zero To Mastery Course.</p>
 		              <a className="btn" href="https://russellsoppe.github.io/Background-Generator" role="button">Go to Website</a>
 	            	</div>
@@ -50,11 +66,14 @@ const WebsitesMF = (props) => {
 		    
 	          <div className="outercard">
 	            <div className="innercard">
+	            	<div className="cardtexttitle" data-aos="fade-down">
+	              	<h1>NavBar Start Up</h1>
+		            </div>
 	            	<div data-aos="fade-up">
-		            	<img alt="NavBar StartUp" href=""></img>
+		            	<img className="webimg" alt="NavBar StartUp" src={navbarsu}></img>
 	            	</div>
 	            	<div className="cardtext" data-aos="fade-down">
-	            		<h1>NavBar Start Up</h1>
+	            		
 		              <p>This website was created with HTML, CSS, and JavaScript.</p>
 		              <a className="btn" href="https://russellsoppe.github.io/navbar-startup/" role="button">Go to Website</a>
 	            	</div>
@@ -63,11 +82,14 @@ const WebsitesMF = (props) => {
 	    
 	    			<div className="outercard">
 	            <div className="innercard">
+	            	<div className="cardtexttitle" data-aos="fade-down">
+	              	<h1>Mobile First Start Up</h1>
+		            </div>
 	            	<div data-aos="fade-up">
-            		 	<img alt="Mobile First Start Up" href=""></img>
+            		 	<img className="webimg" alt="Mobile First Start Up" src={mfnavbar}></img>
                 </div>
                 <div className="cardtext" data-aos="fade-down">
-	            		<h1>Mobile First Start Up</h1>
+	            		
 		              <p>This website was created with HTML, CSS, and JavaScript.</p>
 		              <a className="btn" href="https://russellsoppe.github.io/startupmf/" role="button">Go to Website</a>
 	            	</div>
@@ -77,11 +99,14 @@ const WebsitesMF = (props) => {
 	                 
 	          <div className="outercard">
 		          <div className="innercard">
+		          	<div className="cardtexttitle" data-aos="fade-down">
+	              	<h1>DOM Calculator</h1>
+		            </div>
 		          	<div data-aos="fade-up">
-		              <img alt="Calculator" href=""></img>
+		          		<img className="webimg" alt="Calculator" src={calculator}></img>
 	              </div>
 	            	<div className="cardtext" data-aos="fade-down">
-	            		<h1>DOM Calculator</h1>
+	            		
 		              <p>This website was created with HTML, CSS, and JavaScript. 
 		              The current style choices are from my daughter who loves all things pink and purple.</p>
 		              <a className="btn" href="https://russellsoppe.github.io/calculator/" role="button">Go to Website</a>
