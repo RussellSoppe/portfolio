1e6229cf580303dabb1e189241d0381f2f10976c
russellicious <42648750+russellicious@users.noreply.github.com>
2018-10-22 07:45:19 -0500

    fixed space after main img, altered some of the @media styles

---
+++ src/components/TestBanner.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import calculator from './images/calculator.png';
+
+const TestBanner = ()=>{
+	return (
+		<div>
+			<div className="box">
+      </div>
+      <img className="keyboardimg" src={calculator} alt="abstract smoke swirls">
+      </img>
+      <div className="box">
+      </div>
+    </div>
+	);
+}
+
+export default TestBanner;
+
+		
