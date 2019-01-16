e4987eb5fba96503c60344ed334db0be8ebdffd7
russellicious <42648750+russellicious@users.noreply.github.com>
2019-01-16 14:35:40 -0600

    Updates

---
+++ precache-manifest.4cf19adcf08ad19090ce1405b3e36cf2.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "b227d3b1d98b274c466bd46b4b292dd2",
+    "url": "/portfolio/static/media/huddle.b227d3b1.png"
+  },
+  {
+    "revision": "ba9e0c56ffff5eccddd7",
+    "url": "/portfolio/static/css/main.c76fd27b.chunk.css"
+  },
+  {
+    "revision": "87727d5e4b6e36480d453f4956a55677",
+    "url": "/portfolio/static/media/abstract-art-artistic-1020315.87727d5e.jpg"
+  },
+  {
+    "revision": "8c70deb5723a9c875f5e",
+    "url": "/portfolio/static/js/1.8c70deb5.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "5026d0705605fabc64aa0b7a89ade13c",
+    "url": "/portfolio/static/media/facebrain.5026d070.PNG"
+  },
+  {
+    "revision": "7868fb52b454e59bfe066f41dce6b2b7",
+    "url": "/portfolio/static/media/swapi.7868fb52.PNG"
+  },
+  {
+    "revision": "d2f50d8148f0434bbe349896d0482841",
+    "url": "/portfolio/static/media/mfnavbar.d2f50d81.PNG"
+  },
+  {
+    "revision": "568231e743ff5d7bb8412f16a44064a8",
+    "url": "/portfolio/static/media/robofriends.568231e7.png"
+  },
+  {
+    "revision": "ba9e0c56ffff5eccddd7",
+    "url": "/portfolio/static/js/main.ba9e0c56.chunk.js"
+  },
+  {
+    "revision": "b4b8152f6d57a9c26970ca63db8f8989",
+    "url": "/portfolio/static/media/mealplanning.b4b8152f.png"
+  },
+  {
+    "revision": "b40b0c78438701c892beb3d6b0820907",
+    "url": "/portfolio/static/media/backgen.b40b0c78.PNG"
+  },
+  {
+    "revision": "81bcf51c935536e5c7f3e7359fb70a2b",
+    "url": "/portfolio/static/media/navbarsu.81bcf51c.PNG"
+  },
+  {
+    "revision": "1b3c960d08efc9007f23a520f1b6db25",
+    "url": "/portfolio/static/media/calculator.1b3c960d.png"
+  },
+  {
+    "revision": "144970e4f38712316123987ca2d68d9a",
+    "url": "/portfolio/static/media/keyboard800x1267.144970e4.jpg"
+  },
+  {
+    "revision": "79360cab62af3b62a3a1dd52f1547076",
+    "url": "/portfolio/static/media/keyboard500x792.79360cab.jpg"
+  },
+  {
+    "revision": "180e8e989a9e62c54a54e2169be2f0b8",
+    "url": "/portfolio/static/media/bartosz-kwitkowski-509565-unsplash.180e8e98.jpg"
+  },
+  {
+    "revision": "8c70deb5723a9c875f5e",
+    "url": "/portfolio/static/css/1.4ee76612.chunk.css"
+  },
+  {
+    "revision": "5c16b2a354c69f7ee36f8d5285f9b607",
+    "url": "/portfolio/index.html"
+  }
+];
